78e8365b7c0234a943dbf9d15d2dcd2eb61cb3ca
zuleny <zuleny.cr@gmail.com>
2022-01-18 11:56:30 -0400

    adding default users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,8 +25,10 @@ class CreateUsersTable extends Migration
             $table->string('tipo_usuario',1); 
         });
 
-        DB::table('users')->insert(array('ci' => '9719822','nombre'=>'zuleny','telefono'=>'76431901','email'=>'zuleny@gmail.com','estado'=>'1','password' => '11235813','direccion' => 'Avenida Banzer', 'tipo_usuario'=>'E'));
-        
+        DB::table('users')->insert(array('ci' => '824562091','nombre'=>'admin','telefono'=>'68627871','email'=>'admin@gmail.com','estado'=>'1','password' => '$2y$10$aYdASNvadGXDHLnDCVbKKesg33lg5oegHeQlCC8CeFotp7j4hWKau','direccion' => 'Avenida Alemana y 4to anillo', 'tipo_usuario'=>'E'));
+        DB::table('users')->insert(array('ci' => '9719822','nombre'=>'zuleny','telefono'=>'76431901','email'=>'zuleny.cr@gmail.com','estado'=>'1','password' => '$2y$10$x4rfS1iYXv65UxifZKuqHu38X/h5/2uYYZfQg833IIp1IfIxjJwZS','direccion' => 'Avenida Banzer', 'tipo_usuario'=>'E'));
+        DB::table('users')->insert(array('ci' => '87634201','nombre'=>'ruddy','telefono'=>'69025431','email'=>'ruddyq18@gmail.com','estado'=>'1','password' => '$2y$10$x4rfS1iYXv65UxifZKuqHu38X/h5/2uYYZfQg833IIp1IfIxjJwZS','direccion' => 'Avenida Banzer', 'tipo_usuario'=>'E'));
+        DB::table('users')->insert(array('ci' => '72543190','nombre'=>'stephani','telefono'=>'79025431','email'=>'stephani.hc@gmail.com','estado'=>'1','password' => '$2y$10$x4rfS1iYXv65UxifZKuqHu38X/h5/2uYYZfQg833IIp1IfIxjJwZS','direccion' => 'Avenida Banzer', 'tipo_usuario'=>'E'));
     }
 
     /**
