ff179143dea6bcdb24044b8547696b716f2305dc
stephaniHC <stephani.hc.97@gmail.com>
2022-01-20 08:41:16 -0400

    migracion asistenciaSocio

---
+++ app/Http/Controllers/AsistenciaSocioController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AsistenciaSocio;
+use Illuminate\Http\Request;
+
+class AsistenciaSocioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AsistenciaSocio  $asistenciaSocio
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AsistenciaSocio $asistenciaSocio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AsistenciaSocio  $asistenciaSocio
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AsistenciaSocio $asistenciaSocio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\AsistenciaSocio  $asistenciaSocio
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AsistenciaSocio $asistenciaSocio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AsistenciaSocio  $asistenciaSocio
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AsistenciaSocio $asistenciaSocio)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_01_20_040109_create_asistencia_socio_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAsistenciaSocioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asistencia_socio', function (Blueprint $table) {
+            $table->integer('ci_socio')->unsigned();
+            $table->foreign('ci_socio')->references('ci')->on('users');
+            $table->integer('id_asistencia')->unsigned();
+            $table->foreign('id_asistencia')->references('id')->on('asistencia');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asistencia_socio');
+    }
+}
